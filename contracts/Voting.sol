fe2e3ad83e09d6390e0eda6be3f773f2515b1c6b
Mohammad Nasser <info.hashemabadi@gmail.com>
2025-04-15 18:00:30 +0100

    Update Voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Voting {
+
     struct Poll {
         string question;
         string[] options;
@@ -16,9 +17,10 @@ contract Voting {
     event PollCreated(uint pollId, string question);
     event Voted(uint pollId, address voter, uint option);
 
+    // Create a poll with a question and a set of options
     function createPoll(string memory _question, string[] memory _options) public {
         require(_options.length >= 2, "At least 2 options required");
-        
+
         Poll storage p = polls[pollCount];
         p.question = _question;
         p.options = _options;
@@ -28,6 +30,7 @@ contract Voting {
         pollCount++;
     }
 
+    // Cast a vote for a specific poll and option
     function vote(uint _pollId, uint _option) public {
         Poll storage p = polls[_pollId];
         require(p.exists, "Poll does not exist");
@@ -40,16 +43,19 @@ contract Voting {
         emit Voted(_pollId, msg.sender, _option);
     }
 
+    // Get the number of votes for a specific option in a poll
     function getVotes(uint _pollId, uint _option) public view returns (uint) {
         Poll storage p = polls[_pollId];
         require(p.exists, "Poll does not exist");
         return p.votes[_option];
     }
 
+    // Get all options for a specific poll
     function getOptions(uint _pollId) public view returns (string[] memory) {
         return polls[_pollId].options;
     }
 
+    // Get the question for a specific poll
     function getQuestion(uint _pollId) public view returns (string memory) {
         return polls[_pollId].question;
     }
